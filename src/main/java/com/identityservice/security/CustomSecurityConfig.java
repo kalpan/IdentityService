73767d1d980a795e1b1e1d0e503790c5c07473e9
Kaya Alpan <kaya.alpan@gmail.com>
2018-05-01 23:07:52 -0700

    @EnableWebSecurity seems to be obsolete.

--- src/main/java/com/identityservice/security/CustomSecurityConfig.java
+++ src/main/java/com/identityservice/security/CustomSecurityConfig.java
@@ -5,14 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @SuppressWarnings("deprecation")
 @Configuration
-@EnableWebSecurity
 public class CustomSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	/** A realm is a credential store that enables identity or role based access control. **/
